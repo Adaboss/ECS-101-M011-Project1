f1f83e5c584ce7f58725d29f34ce40ea2ef236ec
Adam <adatron3457@gmail.com>
2023-10-04 10:12:07 -0400

    file opening for encoding.py and decoding.py

--- decoding.py
+++ decoding.py
@@ -1,1 +1,7 @@
-#sample text
+
+with open('byteMap.csv', 'r') as byteMapFile:
+    byteMap = byteMapFile.read()
+
+with open('encoded.txt', 'r') as encodedFile:
+    encoded = encodedFile.read()
+    encoded.strip()
--- encoding.py
+++ encoding.py
@@ -1,2 +1,8 @@
-#sample text
-
+with open('byteMap.csv', 'r') as byteMapFile:
+    byteMap = byteMapFile.read()
+with open('original.txt', 'r') as originalFile:
+    original = originalFile.read()
+    original.strip()
+with open('encoded.txt', 'w+b') as encodedFile:
+    encoded = encodedFile.read()
+    encoded.strip()
