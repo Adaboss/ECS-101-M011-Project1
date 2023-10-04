b0a5692b6bb4c19d543622587809145c8a10bd02
Adam <adatron3457@gmail.com>
2023-10-04 14:29:37 -0400

    adding spaces

--- encoding.py
+++ encoding.py
@@ -1,8 +1,10 @@
 with open('byteMap.csv', 'r') as byteMapFile:
     byteMap = byteMapFile.read()
+
 with open('original.txt', 'r') as originalFile:
     original = originalFile.read()
     original.strip()
+
 with open('encoded.txt', 'w+b') as encodedFile:
     encoded = encodedFile.read()
     encoded.strip()
