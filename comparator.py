9fafb9a02db7a0f699c480541725f3f01f231605
Adam Rogers <adatron3457@gmail.com>
2023-10-03 20:10:54 -0400

    reformatted

--- comparator.py
+++ comparator.py
@@ -3,10 +3,11 @@ import os
 errors = 0
 correctVal = 0
 
-with open ('encoded.txt', 'r') as encodedTxt:
+with open('encoded.txt', 'r') as encodedTxt:
     encoded = encodedTxt.read()
     encoded_file_size = os.path.getsize(encoded)
-with open ('decoded.txt', 'r') as decodedTxt:
+
+with open('decoded.txt', 'r') as decodedTxt:
     decoded = decodedTxt.read()
     decoded.strip()
 
@@ -14,15 +15,15 @@ with open('original.txt', 'r') as originalTxt:
     original = originalTxt.read()
     original.strip()
     original_file_size = os.path.getsize(original)
+
 for i in range(len(original)):
 
-    if original[i]!=decoded[i]:
+    if original[i] != decoded[i]:
         errors += 1
     else:
         correctVal += 1
 
-
-correctness = correctVal/len(original)*100
-compressionRate = encoded_file_size/original_file_size
+correctness = correctVal / len(original) * 100
+compressionRate = encoded_file_size / original_file_size
 print(f"Correctness: {correctness}")
 print(f"Compression Rate: {compressionRate}")
