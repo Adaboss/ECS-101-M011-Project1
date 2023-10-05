e13075cf8a97be09a5b98a31ef90b537b7bbd3cf
Adam <adatron3457@gmail.com>
2023-10-05 11:54:34 -0400

    adding csv-editor plugin; additions to encoding.py; byteMap.csv;

--- decoding.py
+++ decoding.py
@@ -1,7 +1,3 @@
-
-with open('byteMap.csv', 'r') as byteMapFile:
-    byteMap = byteMapFile.read()
-
 with open('encoded.txt', 'r') as encodedFile:
     encoded = encodedFile.read()
     encoded.strip()
--- encoding.py
+++ encoding.py
@@ -1,10 +1,18 @@
-with open('byteMap.csv', 'r') as byteMapFile:
-    byteMap = byteMapFile.read()
+import csv
+
+byteMapFile = open('byteMap.csv')
+byteMap = csv.reader(byteMapFile)
 
 with open('original.txt', 'r') as originalFile:
     original = originalFile.read()
     original.strip()
 
-with open('encoded.txt', 'w+b') as encodedFile:
-    encoded = encodedFile.read()
-    encoded.strip()
+encoded = open('encoded.txt', 'wb')
+
+for letter in range(len(original)):
+
+    for row in byteMap:
+
+        if original[letter] == row[0]:
+            encoded.write(row[2])
+
