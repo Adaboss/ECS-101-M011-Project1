3e9992f10b1de1a8250fc0bd83db9a7faff12f61
Adam <adatron3457@gmail.com>
2023-10-17 10:42:08 -0400

    writes the length of the encoded file before the binary string in encoded.py

--- encoding.py
+++ encoding.py
@@ -4,13 +4,17 @@ with open('byteMapTest.csv') as byteMapFile:
     byteMapReader = csv.reader(byteMapFile)
     byteMap = list(byteMapReader)
 
-with open('original.txt', 'r') as originalFile:
+with open('example1.txt', 'r') as originalFile:
     original = originalFile.read()
-    original.strip()
-encoded = open('encoded.txt', 'w')
+temp = open('temp.txt', 'w')
 
 for letter in range(len(original)):
     for row in byteMap[1:]:
         if original[letter] == row[0]:
-            encoded.write(row[2])
+            temp.write(row[2])
+encoded = open('encoded.txt', 'w')
+temp = open('temp.txt', 'r')
+temp_string = temp.read()
+temp_len = str(len(temp_string))
+encoded.write(temp_len + '.' + temp_string)
 encoded.close()
